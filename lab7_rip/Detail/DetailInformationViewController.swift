36867356f02b6a2960a33512c66eb4ef7d073bf1
AnGLiHaNiN <vaskin200333@gmail.com>
2023-12-27 09:05:59 +0300

    Add native app

---
+++ lab7_rip/Detail/DetailInformationViewController.swift
@@ -0,0 +1,91 @@
+//
+//  DetailInformationViewController.swift
+//  yourProjectName
+//
+//  Created by Mikhail on 11.12.2023.
+//
+
+import UIKit
+
+class DetailInformationViewController: UIViewController {
+    private let  titleLabel = UILabel()
+    private let  descriptionaLabel = UILabel()
+    private let  factoryLabel = UILabel()
+    private let photoImageView = CustomImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed.withAlphaComponent(0.5)
+        [photoImageView,descriptionaLabel,titleLabel,factoryLabel].forEach {
+            view.addSubview($0)
+        }
+        
+        setVisualAppearance()
+        setupImage()
+        setDescriptionLabel()
+        setfactoryLabel()
+        setTitleLabel()
+    }
+}
+
+//MARK: - methods
+extension DetailInformationViewController {
+    func datailConfigure(with model: MedicineUIModel) {
+
+//        print(model.imageUrl.replacingOccurrences(of: "localhost", with: "172.20.10.2"))
+        guard let photoUrl = URL(string: "http://\(model.imageUrl)") else {
+            print("Не получилось создать url")
+            return
+        }
+
+        photoImageView.loadImage(with: photoUrl)
+        titleLabel.text = model.name
+        descriptionaLabel.text = model.dosage
+        factoryLabel.text = model.manufacturer
+    }
+}
+
+//MARK: - private methods
+private extension DetailInformationViewController {
+    func setVisualAppearance() {
+        photoImageView.contentMode = .scaleAspectFit
+        photoImageView.clipsToBounds = true
+        [factoryLabel, descriptionaLabel, titleLabel].forEach {
+            $0.numberOfLines = 0
+            $0.font = UIFont(name: "Times New Roman", size: 20)
+        }
+    }
+
+    func setupImage() {
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        photoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true
+        photoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        photoImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        photoImageView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+    }
+
+    func setTitleLabel() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 15).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        titleLabel.sizeToFit()
+    }
+
+    func setDescriptionLabel() {
+        descriptionaLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionaLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50).isActive = true
+        descriptionaLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        descriptionaLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        descriptionaLabel.sizeToFit()
+    }
+    
+    func setfactoryLabel() {
+        factoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        factoryLabel.topAnchor.constraint(equalTo: descriptionaLabel.bottomAnchor, constant: 50).isActive = true
+        factoryLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        factoryLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        factoryLabel.sizeToFit()
+    }
+}
